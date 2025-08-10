b29404ee27a873653aca2aee6a17364e0e4e6d12
jorgesislema <79588110+jorgesislema@users.noreply.github.com>
2025-08-09 19:19:37 -0500

    Create test_converter.py

---
+++ tests/test_converter.py
@@ -0,0 +1,7 @@
+import unittest
+from src.modules.file_management import converter
+
+class TestConverter(unittest.TestCase):
+    def test_convertir_csv_excel(self):
+        # Aquí probamos la conversión entre CSV y Excel
+        self.assertTrue(True)
