46453c88a9023d021d1dc0ed514950889f85fd57
jorgesislema <79588110+jorgesislema@users.noreply.github.com>
2025-08-09 19:19:42 -0500

    Create test_qr_generator.py

---
+++ tests/test_qr_generator.py
@@ -0,0 +1,7 @@
+import unittest
+from src.modules.code_generators import qr_generator
+
+class TestQRGenerator(unittest.TestCase):
+    def test_generar_qr(self):
+        # Aquí probamos la función de generación de QR
+        self.assertTrue(True)
